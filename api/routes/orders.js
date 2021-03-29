242a93689f0e9cb721cb017c9780f2df5cff6fdc
Anastasia Titova <nastyatitova3538@gmail.com>
2021-03-29 14:52:25 -0700

    prevented CORS errors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,14 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    }
+
     res.status(201).json({
-        message: 'Handling POST requstion for /orders'
+        message: 'Handling POST requstion for /orders',
+        order: order
     })
 })
 
